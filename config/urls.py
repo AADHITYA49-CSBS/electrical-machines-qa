75c45485ac8839e159b3cb2186df6259ad3b908e
Aadhitya.S <150918136+AADHITYA49-CSBS@users.noreply.github.com>
2026-05-30 16:22:41 +0530

    Implement user registration and login system

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from qa import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
+
+
